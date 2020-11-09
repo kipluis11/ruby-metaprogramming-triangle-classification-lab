e8b316ce4cbe45a72d80b7341ff6221e56838595
kip_parcell <kipluis@gmail.com>
2020-11-09 11:51:56 -0700

    commit tree 9028096f79df45bc27af82896a092d6a1ff7d7a8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_accessor :l, :r, :c 
+
+  def initialize(l, r, c)
+    @l = l
+    @r = r
+    @c = c 
+  end
+
+  def kind
+    validate_triangle
+    if l == r && r == c 
+      :equilateral
+    elsif l == r || r == c || l == c 
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(l + r > c), (l + c > r), (r + c > l)]
+    [l, r, c].each do |side|
+      real_triangle << false if side <= 0
+      raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
